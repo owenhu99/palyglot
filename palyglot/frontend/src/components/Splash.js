59201e0050055951e63a240157d5865b5c136b58
farazsfh <farazhussain2000@gmail.com>
2020-11-21 20:04:20 -0500

    added splash page

--- palyglot/frontend/src/components/Splash.js
+++ palyglot/frontend/src/components/Splash.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import '../css/Splash.css';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 
 export default function Splash() {
+    const StyledButton = withStyles({
+        root: {
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          borderRadius: 3,
+          border: 0,
+          color: 'white',
+          height: 48,
+          padding: '0 30px',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        },
+        label: {
+          textTransform: 'capitalize',
+        },
+      })(Button);
+
     return (
-        <div>
-            <h1>Splash</h1>
-        </div>
-    )
+        <>
+            <h1 className="splashTitle">palyglot</h1>
+            <h2 className="splashHeadline">Learn a New Language. Make Some Friends.</h2>
+            <div className="splashButtonsGroup">
+                <div className="splashButton">
+                    <StyledButton component={Link} to={'/login'} variant="contained" color="white" className="splashLogIn">
+                        Log In
+                    </StyledButton>
+                </div>
+                <div className="splashButton">
+                    <StyledButton component={Link} to={'/signup'} variant="contained" color="white" className="splashSignUp">
+                        Sign Up
+                    </StyledButton>
+                </div>
+            </div>
+        </>
+    );
 }

f5c58083f554a8ce3948f0e6e222b9fdcb3688b4
farazsfh <farazhussain2000@gmail.com>
2020-11-22 12:44:11 -0500

    added private route wrapper and modified links

--- palyglot/frontend/src/components/ProfilePage.js
+++ palyglot/frontend/src/components/ProfilePage.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../css/App_Profile.css';
-import '../index.css';
 import NavBar from "./NavBar"
 import Security from "./Security"
 import Profile from "./Profile"
+import { useAuth } from '../contexts/AuthContext';
 
 function ProfilePage() {
+  const { currentUser } = useAuth();
+  const [userDetails, setUserDetails] = useState({});
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_BACKEND_URL + "users", {
+      userId: currentUser.uid,
+      name: name,
+      email: email,
+      gender: gender,
+      age: age,
+      knownLanguages: knownLanguages,
+      targetLanguages: targetLanguages
+    });
+  }, []);
+
   return (
     <div className="page" style={{width: "100%", marginTop: "0"}}>
       <NavBar/>
       <div className="body">
-        <Profile />
-        <Security />
+        <Profile uid={currentUser.uid}/>
+        <Security uid={currentUser.uid}/>
       </div>
     </div>
   );

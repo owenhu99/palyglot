7d0a8ea5c1047653c114ecdbc525b95ed78712e1
farazsfh <farazhussain2000@gmail.com>
2020-11-21 20:28:09 -0500

    more splash page styling and restructuring chat page

--- palyglot/frontend/src/App.js
+++ palyglot/frontend/src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Splash from "./components/Splash";
+import ChatPage from "./components/ChatPage";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -26,6 +27,7 @@ function App() {
               <Route exact path="/" component={Splash} />
               <Route path="/signup" component={SignUp} />
               <Route path="/login" component={LogIn} />
+              <Route path="/chat" component={ChatPage} />
             </Switch>
           </AuthProvider>
         </Router>

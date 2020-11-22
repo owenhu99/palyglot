3263af8163bf70651c753151fed0ea8a815d562d
dom <dominiccasillano710@gmail.com>
2020-11-22 03:53:16 -0500

    finished outline for showing messages from a user's conversations

--- palyglot/frontend/src/components/Chat.js
+++ palyglot/frontend/src/components/Chat.js
@@ -1,60 +1,73 @@
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, InsertEmoticon, SearchOutlined } from "@material-ui/icons";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "../css/Chat.css";
 
 // function Chat({message}) {
-function Chat() {
+function Chat(props) {
 
-    // const [input, setInput] = React.useState("");
+    const [input, setInput] = React.useState("");
 
-    // const sendMessage = (e) => {
-    //     e.preventDefault();
-        
-    //     setInput("");
-    // }
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post("ROUTE_HERE", {
+
+        })
+        setInput("");
+    }
+
+    //     <p className="chat_message">
+    // <span className="chat_name">Owen{/*message.name*/}</span>
+    //     Comment tu di sa "Pizza Parlour" en Francais? {/*message.message*/}
+    // <span className="chat_timestamp">{new Date().toUTCString()} {/*message.timestamp*/} </span>
+    // </p>
+    // <p className="chat_message chat_receiver">
+    // <span className="chat_name">JJ</span>
+    // Pardon, Owen. Ma Francais c'est tres mauvais.
+    // <span className="chat_timestamp">{new Date().toUTCString()}</span>
+    // </p>
 
     return (
         <div className="chat">
             <div className="chat_header">
                 <Avatar />
-                <div className = "chat_headerInfo">
-                    <h3>Room Name</h3>
+                <div className="chat_headerInfo">
+                    <h3>{props.room.name}</h3>
                     <p>Last seen at...</p>
                 </div>
                 <div className="chat_headerRight">
                     <IconButton>
-                        <SearchOutlined/>
+                        <SearchOutlined />
                     </IconButton>
                     <IconButton>
-                        <AttachFile/>
+                        <AttachFile />
                     </IconButton>
                 </div>
             </div>
             <div className="chat_body">
-                {/* { messages.map((message) => ( */}
-                    <p className="chat_message">
-                        <span className="chat_name">Owen{/*message.name*/}</span>
-                        Comment tu di sa "Pizza Parlour" en Francais? {/*message.message*/}
-                <span className="chat_timestamp">{new Date().toUTCString()} {/*message.timestamp*/} </span>
-                    </p>      
-                {/* ))} */}
-                
-                <p className="chat_message chat_receiver">
-                    <span className="chat_name">JJ</span>
-                    Pardon, Owen. Ma Francais c'est tres mauvais.
-                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
-                </p>
+                {props.messages.map((message) => {
+                    <p className={`chat_message ${message.from !== "USER_ID" && "chat_receiver"}`}
+                    >
+                        <span className="chat_name">
+                            {message.from}
+                        </span>
+                        {message.message}
+                        <span className="chat_timestamp">
+                            {message.date}
+                        </span>
+                    </p>
+                })}
             </div>
             <div className="chat_input">
-                <IconButton><InsertEmoticon/></IconButton>
+                <IconButton><InsertEmoticon /></IconButton>
                 <form>
-                    <input 
-                        // value={input}
-                        // onChange={(e)=>setInput(e.target.value)} 
-                        onChangeplaceholder="écrire un message" 
-                        type="text"/>
-                    <button type="submit">
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="écrire un message"
+                        type="text" />
+                    <button onClick={sendMessage} type="submit">
                         Send Message
                     </button>
                 </form>
--- palyglot/frontend/src/components/ChatPage.js
+++ palyglot/frontend/src/components/ChatPage.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import NavBar from './NavBar';
 import '../css/App_Chat.css';
+import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 export default function ChatPage() {
+
+    const { currentUser } = useAuth();
+    console.log(currentUser);
+    const [currentRoom, setCurrentRoom] = React.useState({});
+    const [rooms, setRooms] = React.useState([]);
+    const [messages, setMessages] = React.useState([]);
+
+    /* Get all of the user's conversations, set the focused conversation to the
+     * latest conversation, and get the past messages for that conversation.
+     */
+    useEffect(() => {
+        axios.get("GET_ROOMS")
+            .then(res => {
+                setRooms(res.data);
+            })
+            .then(() => {
+                setCurrentRoom(rooms[0]);
+            })
+            .then(() => {
+                axios.get("GET_ROOM_MESSAGE")
+                    .then(res => {
+                        setMessages(res.data);
+                    })
+            })
+    }, [rooms]);
+
+    function changeRoom(roomId) {
+        getRoomMessages(roomId);
+        setCurrentRoom(roomId);
+    }
+
+    function getRoomMessages(roomId) {
+        axios.get("GET_ROOM_MESSAGE")
+            .then(res => {
+                setMessages(res.data);
+            })
+    }
+
     return (
         <div className="App">
             <NavBar></NavBar>
             <div className="body">
-                <Sidebar></Sidebar>
-                <Chat></Chat>
+                <Sidebar rooms={rooms} currentRoom={currentRoom}
+                    onChangeRoom={changeRoom}>
+                </Sidebar>
+                <Chat room={currentRoom} messages={messages}></Chat>
             </div>
         </div>
     );

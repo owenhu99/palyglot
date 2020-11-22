06ca92f292061c855fc417f7399fee9e1407d6c8
dom <dominiccasillano710@gmail.com>
2020-11-21 20:31:22 -0500

    commit for merge

--- palyglot/frontend/src/App.js
+++ palyglot/frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Palyglot</h1>
+      <div className="app">
+        <NavBar></NavBar>
+      </div>
+      <div className="app_body">
+        <Sidebar></Sidebar>
+        <Chat></Chat>
+      </div>
     </div>
   );
 }

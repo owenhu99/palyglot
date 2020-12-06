1b2ef971b615af6728e5cf1b7d67543e78614496
Owen Hu <owenhu99@gmail.com>
2020-12-06 01:51:58 -0500

    issue 60: remove old auth token generation

--- palyglot/backend/routes/users.js
+++ palyglot/backend/routes/users.js
@@ -18,8 +18,7 @@ router.post("/", async (req, res) => {
 	try {
 		const user = new User(req.body)
 		await user.save()
-		const token = await user.generateAuthToken()
-		return res.status(201).send({ user, token })
+		return res.status(201).send(user)
 	} catch (error) {
 		return res.status(400).send(error)
 	}

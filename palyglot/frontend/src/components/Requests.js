77aa4785dd263b3c874324316db5158e091b8d77
farazsfh <farazhussain2000@gmail.com>
2020-12-08 13:37:07 -0500

    added requests page, copy of matchmaking

--- palyglot/frontend/src/components/Requests.js
+++ palyglot/frontend/src/components/Requests.js
@@ -3,19 +3,18 @@ import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import '../css/Matchmaker.css'
 import NavBar from './NavBar';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
-import { Alert, AlertTitle } from '@material-ui/lab';import CheckIcon from '@material-ui/icons/Check';
+import { Alert } from '@material-ui/lab';import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
 import { useAuth } from '../contexts/AuthContext';
 import { makeStyles } from '@material-ui/core/styles';
 
 const axios = require('axios').default;
 
-function Matchmaker() {
+function Requests() {
   const useStyles = makeStyles((theme) => ({
     root: {
     },
@@ -30,59 +29,59 @@ function Matchmaker() {
 
   const classes = useStyles();
   const { currentUser } = useAuth();
-  const [matches, setMatches] = useState([]);
-  const [currMatch, setCurrMatch] = useState(0);
+  const [requestIds, setRequestIds] = useState([]);
+  const [currRequest, setCurrRequest] = useState({name: "", gender: "", age: "", knownLanguages: [], interests: [], profilePicture: ""});
+  const [currRequestId, setCurrRequestId] = useState(0);
   const [info, setInfo] = useState("");
   
   useEffect(() => {    
     currentUser.getIdToken(true).then((idToken) => {
-      axios.get("http://localhost:5000/matchmaking/", {
+      axios.get("http://localhost:5000/users/me", {
         headers: {
           'Authorization': `Bearer ${idToken}`
         }
       }).then((response) => {
-          setMatches(response.data.matches);
-          console.log(response.data.matches);
+          setRequestIds(response.data.matchInvites)
       });
     }).catch((error) => {
       console.log(error);
     });
   }, []);
 
-  const dontMatchUser = () => {
-    setCurrMatch(currMatch + 1);
-    setInfo("");
-  }
+//   const dontMatchUser = () => {
+//     setCurrMatch(currMatch + 1);
+//     setInfo("");
+//   }
 
-  const matchUser = (toUserId) => {
-    console.log("here")
-    currentUser.getIdToken(true).then((idToken) => {
-      axios.post("http://localhost:5000/matchmaking/requestMatch", {
-        toUser: toUserId
-      }, {
-        headers: {
-          'Authorization': `Bearer ${idToken}`
-        }
-      }).then((response) => {
-          console.log(response);
-          setInfo(response.data.msg);
-      }, (error) => {
-        console.log(error);
-        setInfo("Whoops, match invite could not be sent.");
-      });
-    }).catch((error) => {
-      console.log(error);
-    });
+//   const matchUser = (toUserId) => {
+//     console.log("here")
+//     currentUser.getIdToken(true).then((idToken) => {
+//       axios.post("http://localhost:5000/matchmaking/requestMatch", {
+//         toUser: toUserId
+//       }, {
+//         headers: {
+//           'Authorization': `Bearer ${idToken}`
+//         }
+//       }).then((response) => {
+//           console.log(response);
+//           setInfo(response.data.msg);
+//       }, (error) => {
+//         console.log(error);
+//         setInfo("Whoops, match invite could not be sent.");
+//       });
+//     }).catch((error) => {
+//       console.log(error);
+//     });
 
-    setCurrMatch(currMatch + 1);
-  }
+//     setCurrMatch(currMatch + 1);
+//   }
 
-  if (currMatch >= matches.length) {
+  if (currRequestId >= requestIds.length) {
     return (
       <div>
       <NavBar></NavBar>
       <div class="nomatches">
-        <h1>No more matches left.</h1>
+        <h1>No more incoming requests left.</h1>
       </div>
     </div>
     );
@@ -91,19 +90,19 @@ function Matchmaker() {
       <div className="matchmakerPage">
         <NavBar></NavBar>
         <div class="matches">
-          <h1 className="title">Find a Pal</h1>
+          <h1 className="title">Incoming Requests</h1>
           {info && <div className="alert"><Alert severity="info">{info}</Alert></div>}
           <Card className={classes.root}>
             <div className="cardHeader">
               <Typography variant="h5" color="textPrimary" component="h5">
-                {matches[currMatch].name}
+                {currRequest.name}
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                {matches[currMatch].gender + ", " + matches[currMatch].age}
+                {currRequest.gender + ", " + currRequest.age}
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-              {matches[currMatch].knownLanguages.map(function(language, i) {
-                if (i == matches[currMatch].knownLanguages.length - 1) {
+              {currRequest.knownLanguages.map(function(language, i) {
+                if (i == currRequest.knownLanguages.length - 1) {
                   return(
                     language
                   )
@@ -118,21 +117,21 @@ function Matchmaker() {
             </div>
             <CardMedia
               className={classes.media}
-              image={matches[currMatch].profilePicture}
+              image={currRequest.profilePicture}
             />
             <CardContent>
               <Typography variant="body2" color="textPrimary" component="p">
                 Bio
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                {matches[currMatch].bio === "" ? "This user has not set their bio." : matches[currMatch].bio}
+                {currRequest.bio === "" ? "This user has not set their bio." : currRequest.bio}
               </Typography>
               <Typography className="interests" variant="body2" color="textPrimary" component="p">
                 Interests
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                {matches[currMatch].interests.map(function(interest, i) {
-                    if (i == matches[currMatch].interests.length - 1) {
+                {currRequest.interests.map(function(interest, i) {
+                    if (i == currRequest.interests.length - 1) {
                       return(
                         interest
                       )
@@ -146,10 +145,10 @@ function Matchmaker() {
               </Typography>
             </CardContent>
             <CardActions disableSpacing>
-              <IconButton onClick={() => matchUser(matches[currMatch].userId)} aria-label="send match">
+              <IconButton onClick={()=>console.log("clicked")} aria-label="send match">
                 <CheckIcon />
               </IconButton>
-              <IconButton className={classes.clear} onClick={dontMatchUser} aria-label="dont match">
+              <IconButton className={classes.clear} onClick={()=>console.log("clicked")} aria-label="dont match">
                 <ClearIcon />
               </IconButton>
             </CardActions>
@@ -160,4 +159,4 @@ function Matchmaker() {
   }
 }
  
-export default Matchmaker;
+export default Requests;

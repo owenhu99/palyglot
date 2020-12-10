a2de2a673a823c18d87287e15a5c523d59039437
JJ Kanu <jjkanu20@gmail.com>
2020-12-09 19:02:48 -0500

    Added Sidebar Header, slight formatting to colors and borders

--- palyglot/frontend/src/components/Sidebar.js
+++ palyglot/frontend/src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/Sidebar.css";
-import { SearchOutlined } from "@material-ui/icons";
 import SidebarRoom from "./SidebarRoom"
 
 function Sidebar(props) {
     return (
         <div className="sidebar">
+            <div className="sidebar_header">
+                <h3>My Chats</h3>
+            </div>
             <div className="sidebar_rooms">
                 {props.rooms.map((room, i) => {
                     return (
